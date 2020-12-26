fce12ea3fd0d73a27d613a35858c4a99c7a7e6b5
Kihyuk Sung <SamuelSung.dev@gmail.com>
2020-12-26 08:19:45 +0000

    feat: Get jobs from remoteok.io

--- jobscrapper.py
+++ jobscrapper.py
@@ -17,13 +17,9 @@ ro_url = f'https://remoteok.io/remote-dev+'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
 
-#  stackoverflow_search_url = stackoverflow_url+term
-#  weworkremotely_search_url = weworkremotely_url+term
-#  remoteok_search_url = remoteok_url+f'{term}-jobs'
-
 def get_so_jobs(term):
   so_search_url = so_url+term
-  so_r = requests.get(so_search_url)
+  so_r = requests.get(so_search_url, headers=headers)
   print(f'Scrapping : {so_search_url}')
   so_soup = BeautifulSoup(so_r.text, 'html.parser')
   # with open(f'so_{term}.html','w') as f:
@@ -31,7 +27,7 @@ def get_so_jobs(term):
   so_job_total = int(so_soup.find(class_='js-search-title').span.text.split(' ')[0])
   if so_job_total == 0:
     print('No jobs')
-    return
+    return []
   else:
     print(f'Total Jobs: {so_job_total}')
   pagenation = int(so_soup.find(class_='s-pagination').a['title'].split(' ')[-1])
@@ -39,7 +35,7 @@ def get_so_jobs(term):
   for ii in range(pagenation):
     so_job_soup = None
     if ii != 0:
-      so_page_r = requests.get(so_search_url+f'&pg={ii+1}')
+      so_page_r = requests.get(so_search_url+f'&pg={ii+1}', headers=headers)
       so_job_soup = BeautifulSoup(so_page_r.text, 'html.parser')
       pass
     else:
@@ -60,11 +56,11 @@ def get_so_jobs(term):
 
 def get_wwr_jobs(term):
   wwr_search_url = wwr_url+term
-  wwr_r = requests.get(wwr_search_url)
+  wwr_r = requests.get(wwr_search_url, headers=headers)
   print(f'Scrapping : {wwr_search_url}')
   wwr_soup = BeautifulSoup(wwr_r.text, 'html.parser')
   if wwr_soup.find(class_='no_result'):
-    return
+    return []
   wwr_job_list = wwr_soup.find(class_='jobs').find_all('li')
   job_list = []
   for job in wwr_job_list[0:-1]:
@@ -80,7 +76,31 @@ def get_wwr_jobs(term):
       })
   return job_list
 
+def get_ro_jobs(term):
+  ro_search_url = ro_url+f'{term}-jobs'
+  ro_r = requests.get(ro_search_url, headers=headers)
+  print(f'Scrapping : {ro_search_url}')
+  ro_soup = BeautifulSoup(ro_r.text, 'html.parser')
+  if ro_soup.find(class_='center'):
+    print('404')
+    return []
+  
+  ro_job_list = ro_soup.find_all(class_='job')
+  job_list = []
+  for job in ro_job_list:
+    if 'closed' not in job['class']:
+      href = 'https://remoteok.io'+job.find(itemprop='url')['href']
+      company = job.find(itemprop='name').text
+      title = job.find(itemprop='title').text
+      job_list.append({
+        'title':title,
+        'company':company,
+        'href':href,
+      })
+  return job_list
+
 def get_jobs(term):
   # so_jobs = get_so_jobs(term)
-  wwr_jobs = get_wwr_jobs(term)
-  return wwr_jobs
+  # wwr_jobs = get_wwr_jobs(term)
+  ro_jobs = get_ro_jobs(term)
+  return ro_jobs
